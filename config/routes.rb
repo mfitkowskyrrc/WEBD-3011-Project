2edb2fa35c77fd14c977089fef9505b76eeb9fa6
mfitkowsky <mfitkowsky@academic.rrc.ca>
2025-03-30 20:53:30 -0500

    Added home page, and nav bar
    nav bar added via application.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root "home#home"
   resources :order_items
   resources :orders
   resources :events
@@ -15,6 +16,6 @@ Rails.application.routes.draw do
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+
+
 end
