88c3ec9d01ceab9284fb5b654d88b6fdec3e9f62
mfitkowsky <mfitkowsky@academic.rrc.ca>
2025-04-01 19:24:47 -0500

    Created Products in Active Admin

---
+++ app/admin/products.rb
@@ -0,0 +1,15 @@
+ActiveAdmin.register Product do
+
+  permit_params :name, :description, :price, :stock_quantity, :category_id
+
+  form do |f|
+    f.inputs "Product Details" do
+      f.input :name
+      f.input :description
+      f.input :price
+      f.input :stock_quantity
+      f.input :category, as: :select, collection: Category.all.map { |c| [c.name, c.id] }  # Dropdown for categories
+    end
+    f.actions  # Adds 'Create' and 'Cancel' buttons
+  end
+end
--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,11 @@ class Product < ApplicationRecord
   has_many :orders, through: :order_items
 
   validates :name, :description, :price, :stock_quantity, presence: true
+
+  def self.ransackable_associations(auth_object = nil)
+    ["category", "order_items", "orders"]
+  end
+  def self.ransackable_attributes(auth_object = nil)
+    ["category_id", "created_at", "description", "id", "id_value", "name", "price", "stock_quantity", "updated_at"]
+  end
 end
