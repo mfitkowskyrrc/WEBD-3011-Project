1b2420d9f3b72c235eeb42c5bef29484ffdd647b
mfitkowsky <mfitkowsky@academic.rrc.ca>
2025-04-01 19:26:55 -0500

    Created Category in Active Admin

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,11 @@ class Category < ApplicationRecord
   has_many :products, dependent: :destroy
 
   validates :name, presence: true
+
+  def self.ransackable_associations(auth_object = nil)
+    ["products"]
+  end
+  def self.ransackable_attributes(auth_object = nil)
+    ["created_at", "id", "id_value", "name", "updated_at"]
+  end
 end
