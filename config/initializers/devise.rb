6910c8799320127502f5dc2ecf9f655b0fa18311
mfitkowsky <mfitkowsky@academic.rrc.ca>
2025-04-20 11:23:43 -0500

    fixed bug with devise config
    created during last push

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -267,8 +267,6 @@ Devise.setup do |config|
 
   # The default HTTP method used to sign out a resource. Default is :delete.
   config.sign_out_via = :delete
-  config.after_sign_out_path_for = root_path
-
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
